67eb2d4ba6df6b5f3d98c745c7702105ed77104d
Andrew <a.t.andrew.tan@gmail.com>
2019-01-27 12:58:29 -0800

    message to chatbot

--- src/Components/Talk/results.js
+++ src/Components/Talk/results.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 class Results extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       // const app = apiai("7d84fd1d0c27411daec92fde07c8417b");
     };
   }
@@ -25,7 +25,7 @@ class Results extends Component {
     <div id="match-details-curtain">
         <div id="match-details-container">
             <h1 id="statresults">Conversation Results</h1>
-            <h3 id="percentresult">Grammar 98% Correct</h3>
+            <h3 id="percentresult">Grammar 87% Correct</h3>
             <h3 id="duration">Total Duration 3:15</h3>
             <hr id="bottom-divider" />
             <div id="close-details" onClick={()=>{this.props.closestats()}}></div>
