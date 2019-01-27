fe7d47870d48e74cce68d2c17fe3a7843433bc99
Andrew <a.t.andrew.tan@gmail.com>
2019-01-26 21:52:10 -0800

    Text around world

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,17 +14,38 @@ class Home extends Component {
   render() {
   return (
     <div className="big">
-      <div className="center">
-        <h1> Select a Language </h1>
-        <div className="circle">
-          <img src={globe} height="300"/>
-        </div>
-        <p> TALK </p>
-      </div>
+      <img id="circle" src={globe}/>
 
-      <div className="left">
-        <h2> English </h2>
-      </div>
+    <div className="center">
+      <h1> Select a Language </h1>
+    </div>
+
+    <div className="left">
+      <h2><br/></h2>
+      <h2><br/></h2>
+      <h2> English </h2>
+      <h2><br/></h2>
+      <h2> French Français </h2>
+      <h2><br/></h2>
+      <h2> Chinese 中文 </h2>
+      <h2><br/></h2>
+      <h2> Korean 한국어</h2>
+      <h2><br/></h2>
+      <h2> Japanese 日本語 </h2>
+    </div>
+
+    <div className="right">
+        <h2> Russian русский </h2>
+        <h2><br/></h2>
+        <h2> Spanish Español </h2>
+        <h2><br/></h2>
+        <h2> Portuguese português </h2>
+        <h2><br/></h2>
+        <h2> Italian italiano </h2>
+        <h2><br/></h2>
+        <h2> Arabic العَرَبِيَّة </h2>
+    </div>
+
     </div>
     );
   }
