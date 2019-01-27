3e3f65d26fd3a42d4d3c21072d384ef8143d4709
Noor Elramley <noor@dhcp-206-87-220-96.ubcsecure.wireless.ubc.ca>
2019-01-27 02:41:57 -0800

    Home edits

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,18 @@ import globe from '../../Images/planet.png';
 
 
 class Home extends Component {
+
+  state = {
+
+  show_check: false,
+}
+
+clickLanguage() {
+  this.setState({
+  show_check: true,
+  });
+ }
+
  constructor() {
    super();
    this.state={
@@ -14,6 +26,8 @@ class Home extends Component {
  render() {
  return (
    <div className="big">
+
+
      <img id="circle" src={globe}/>
 
      <div className="center-top">
@@ -21,32 +35,33 @@ class Home extends Component {
      </div>
 
      <div className="left">
-       <h2> English </h2>
+       <h2 onClick={(e) =>
+                {this.clickLanguage()}} className="language"> English </h2>
        <h2><br/></h2>
-       <h2> French Français </h2>
+       <h2 className="language"> French Français </h2>
        <h2><br/></h2>
-       <h2> Chinese 中文 </h2>
+       <h2 className="language"> Chinese 中文 </h2>
        <h2><br/></h2>
-       <h2> Korean 한국어</h2>
+       <h2 className="language"> Korean 한국어</h2>
        <h2><br/></h2>
-       <h2> Japanese 日本語 </h2>
+       <h2 className="language"> Japanese 日本語 </h2>
      </div>
 
      <div className="right">
-         <h2> Russian русский </h2>
+         <h2 className="language"> Russian русский </h2>
          <h2><br/></h2>
-         <h2> Spanish Español </h2>
+         <h2 className="language"> Spanish Español </h2>
          <h2><br/></h2>
-         <h2> Portuguese português </h2>
+         <h2 className="language"> Portuguese português </h2>
          <h2><br/></h2>
-         <h2> Italian italiano </h2>
+         <h2 className="language"> Italian italiano </h2>
          <h2><br/></h2>
-         <h2> Arabic العَرَبِيَّة </h2>
+         <h2 className="language"> Arabic العَرَبِيَّة </h2>
      </div>
 
-     <div className="center-bot">
-       <h1> TALK </h1>
-     </div>
+     <span className="spanTalk center-bot"><div className="btnTalk">Begin talking</div></span>
+
+
 
    </div>
    );
