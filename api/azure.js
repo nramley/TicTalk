c83c7533e8f91dedae035abcfef39ccb6496dd19
klynx <KevinLynx_L@hotmail.com>
2019-01-27 04:42:55 -0800

    Added grammar check to apis

--- api/azure.js
+++ api/azure.js
@@ -9,6 +9,8 @@ const readline = require('readline-sync');
 // Requires xmlbuilder to build the SSML body
 const xmlbuilder = require('xmlbuilder');
 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 // Read SubscriptionKey of Azure
 const subscriptionKey = process.env.AZURE_SUBSCRIPTION_KEY;
 if (!subscriptionKey) {
@@ -144,6 +146,70 @@ function speechToText(filename, callback){
         recognizer = undefined;
     });
 }
+
+let bingkey = "75ba4ccffd5d4c1188a2b0e9e1deb713";
+
+let https = require ('https');
+
+let host = 'api.cognitive.microsoft.com';
+let path = '/bing/v7.0/spellcheck';
+
+/* NOTE: Replace this example key with a valid subscription key (see the Prequisites section above). Also note v5 and v7 require separate subscription keys. */
+let key = process.env.BING_SUBSCRIPTION_KEY;
+
+// These values are used for optional headers (see below).
+// let CLIENT_ID = "<Client ID from Previous Response Goes Here>";
+// let CLIENT_IP = "999.999.999.999";
+// let CLIENT_LOCATION = "+90.0000000000000;long: 00.0000000000000;re:100.000000000000";
+
+ 
+const BingSpellChecker = require('bing-spell-checker')
+ 
+BingSpellChecker.init({
+  'key': process.env.BING_SUBSCRIPTION_KEY
+})
+ 
+let promise = BingSpellChecker.check('It workd!')
+promise.then(function (result) {
+  console.log(result) // "Stuff worked!"
+}, function (err) {
+  console.log(err) // Error: "It broke"
+})
+// let mkt = "en-US";
+// let mode = "proof";
+// let text = "Hollo, wrld!";
+// let query_string = "?mkt=" + mkt + "&mode=" + mode;
+
+// let request_params = {
+//     method : 'POST',
+//     hostname : host,
+//     path : path + query_string,
+//     headers : {
+//         'Content-Type' : 'application/x-www-form-urlencoded',
+//         'Content-Length' : text.length + 5,
+//         'Ocp-Apim-Subscription-Key' : key,
+// //        'X-Search-Location' : CLIENT_LOCATION,
+// //        'X-MSEdge-ClientID' : CLIENT_ID,
+// //        'X-MSEdge-ClientIP' : CLIENT_ID,
+//     }
+// };
+
+// let response_handler = function (response) {
+//     let body = '';
+//     response.on ('data', function (d) {
+//         body += d;
+//     });
+//     response.on ('end', function () {
+//         console.log (body);
+//     });
+//     response.on ('error', function (e) {
+//         console.log ('Error: ' + e.message);
+//     });
+// };
+
+// let req = https.request (request_params, response_handler);
+// req.write ("text=" + text);
+// req.end ();
 
 
 module.exports = {
