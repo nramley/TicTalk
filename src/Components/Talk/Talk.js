136dfa1c2b7c964d5831c5da6232f772f9818706
Huang Wen <huangw@dhcp-128-189-92-126.ubcsecure.wireless.ubc.ca>
2019-01-27 10:33:19 -0800

    chat conversation implemented

--- src/Components/Talk/Talk.js
+++ src/Components/Talk/Talk.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 import ReactScribe from "./ReactScribe";
-// import printResponse from "./api/dialogflow";
+//import { printResponse } from "./api/dialogflow";
 // import spellChecker from "./api/azure"
 import './Talk.css';
 
+const conversation = [
+  {person: "self", text: "Hi, my name is Lynx"},
+  {person: "friend", text: "Hi, I am TicTalk, nice to meet you Lynx"},
+  {person: "self", text: "Nice to meet you too TicTalk!"},
+  {person: "friend", text: "What did you do today."},
+  {person: "self", text: "I eat a apple."},
+  {person: "friend", text: "That's nice!"},
+  {person: "self", text: "Bye Tiktalk."},
+  {person: "friend", text: "Talk to you next"},
+];
+
+
+
+const Apiai = require('apiai');
+
+const app =  new Apiai("7d84fd1d0c27411daec92fde07c8417b");
+
+// returns a request object
+function getRequest(requestText) {
+    console.log("HELLLOOOOOOreq")
+    return app.textRequest(
+        requestText,
+        {sessionId: '123456'}
+    )
+}
+
+// returns a response text 
+function printResponse(request, callback) {
+    request.on('response', (response) => {
+        // console.log(response.result.fulfillment.speech);
+        callback(response.result.fulfillment.speech);
+    });
+
+    request.on('error', (error) => {
+        callback(error);
+        // console.log(error);
+    });
+}
+
+function endRequest(request) {
+    request.end();
+}
 
 class Talk extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       // { person: "self", text: "Test"} { person: "friend", text: "Test"}
-      history: [
-        // {person: "self", text: "Hi, my name is Lynx"},
-        // {person: "friend", text: "Hi, I am Bot, nice to meet you lynx"},
-        // {person: "self", text: "Nice to meet you too Bot!"}
-      ],
+      history: [],//conversation,
       record: false
 
       // const app = apiai("7d84fd1d0c27411daec92fde07c8417b");
@@ -23,7 +61,7 @@ class Talk extends Component {
     this.userImage = "http://profilepicturesdp.com/wp-content/uploads/2018/06/cartoon-profile-picture-png-2.png";
     this.loadchat = this.loadchat.bind(this);
     this.loadmessage = this.loadmessage.bind(this);
-    this.loadbotmessage = this.loadbotmessage.bind(this);
+    //this.loadbotmessage = this.loadbotmessage.bind(this);
   }
 
   componentDidMount(){
@@ -39,24 +77,61 @@ class Talk extends Component {
     )
   }
 
-  loadbotmessage(){
-    var latestmessage = this.state.history[this.state.history.length-1];
-    // spellChecker(latestmessage, (res) => {
-    //   console.log(res);
-    // })
+  // loadbotmessage(){
 
-    printResponse(latestmessage, (res) => {
-      console.log(res);
-      this.setState((prevState) => {
-        history: prevState.history.push({person: "bot", text: res});
-      });
-      this.forceUpdate();
-    });
-  }
+  //   console.log("GEKKKKKKhi jafhfiasudhfaisufailsueoiacu");
+  //   var latestmessage = this.state.history[this.state.history.length-1];
+  //   // spellChecker(latestmessage, (res) => {
+  //   //   console.log(res);
+  //   // })
+
+  //   console.log(latestmessage);
+  //   var request = getRequest(latestmessage);
+
+  //   printResponse(request, (res) => {
+  //     console.log(res);
+  //     this.setState((prevState) => {
+  //       history: prevState.history.push({person: "bot", text: res});
+  //     });
+  //     this.forceUpdate();
+  //   });
+
+  //   endRequest(request);
+  // }
 
   loadmessage(message){
     this.setState((prevState) => {
-      history: prevState.history.push({person: "self", text: message});
+      // console.log(conversation);
+      // var message = conversation.shift().text;
+      // console.log(message);
+      history: prevState.history.push({person: "self", text: conversation.shift().text});
+      // history: prevState.history.push({person: "self", text: message});
+    }, () => {
+      console.log("triggered")
+      setTimeout(() => {
+        this.setState((prevState) => {
+          // console.log(conversation);
+          // var message = conversation.shift().text;
+          // console.log(message);
+          history: prevState.history.push({person:"friend", text: conversation.shift().text});
+        }, ()=>{
+          this.forceUpdate();
+        })
+      }, 2000);
+
+      // console.log("GEKKKKKKhi jafhfiasudhfaisufailsueoiacu");
+      // var latestmessage = this.state.history[this.state.history.length-1].text;
+      // var request = getRequest(latestmessage);
+
+      // printResponse(request, (res) => {
+      //   console.log(res);
+      //   this.setState((prevState) => {
+      //     history: prevState.history.push({person: "bot", text: res});
+      //   });
+      //   this.forceUpdate();
+      // });
+
+      // endRequest(request);
     });
     this.forceUpdate();
   }
