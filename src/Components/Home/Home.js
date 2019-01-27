bcbdb8fc5bc0872bda2fed46dbce88cb9192bc8a
Noor Elramley <noor@dhcp-206-87-220-96.ubcsecure.wireless.ubc.ca>
2019-01-27 03:41:54 -0800

    Added orange2 class

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -32,27 +32,27 @@ class Home extends Component {
      </div>
 
      <div className="left">
-       <h2 className={`language ${option == "English" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>English</h2>
+       <h2 className={`language ${option == "English" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>English</h2>
        <h2><br/></h2>
-       <h2 className={`language ${option == "French Français" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>French Français</h2>
+       <h2 className={`language ${option == "French Français" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>French Français</h2>
        <h2><br/></h2>
-       <h2 className={`language ${option == "Chinese 中文" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Chinese 中文</h2>
+       <h2 className={`language ${option == "Chinese 中文" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Chinese 中文</h2>
        <h2><br/></h2>
-       <h2 className={`language ${option == "Korean 한국어" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Korean 한국어</h2>
+       <h2 className={`language ${option == "Korean 한국어" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Korean 한국어</h2>
        <h2><br/></h2>
-       <h2 className={`language ${option == "Japanese 日本語" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Japanese 日本語</h2>
+       <h2 className={`language ${option == "Japanese 日本語" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Japanese 日本語</h2>
      </div>
 
      <div className="right">
-         <h2 className={`language ${option == "Russian русский" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Russian русский</h2>
+         <h2 className={`language ${option == "Russian русский" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Russian русский</h2>
          <h2><br/></h2>
-         <h2 className={`language ${option == "Spanish Español" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Spanish Español</h2>
+         <h2 className={`language ${option == "Spanish Español" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Spanish Español</h2>
          <h2><br/></h2>
-         <h2 className={`language ${option == "Portuguese português" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Portuguese português</h2>
+         <h2 className={`language ${option == "Portuguese português" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Portuguese português</h2>
          <h2><br/></h2>
-         <h2 className={`language ${option == "Italian italiano" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Italian italiano</h2>
+         <h2 className={`language ${option == "Italian italiano" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Italian italiano</h2>
          <h2><br/></h2>
-         <h2 className={`language ${option == "Arabic العَرَبِيَّة" ? 'orange' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Arabic العَرَبِيَّة</h2>
+         <h2 className={`language ${option == "Arabic العَرَبِيَّة" ? 'orange2' : ''}`} onClick={this.handleOptionsChange.bind(this)}>Arabic العَرَبِيَّة</h2>
      </div>
 
      <span className="spanTalk center-bot"><div className="btnTalk">Begin talking</div></span>
